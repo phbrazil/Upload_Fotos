35db3cf4cf3664a56abce0cebf5197f8a4a0ddec
Paulo Bezerra <pauloh2012sul@gmail.com>
2020-12-29 17:09:45 -0300

    versao beta

--- src/main/webapp/js/fotosHome.js
+++ src/main/webapp/js/fotosHome.js
@@ -17,7 +17,7 @@ function GetFotosHome() {
             for (var k = 0; k < (json.length); k++) {
 
                 var images = '<a href="#modal' + json[k].id + '" data-toggle="modal" data-target="#modal' + json[k].id + '">' +
-                        '<img src="http://34.71.7.57' + json[k].path + '" alt="">' +
+                        '<img src="https://www.cyberoficina.com.br' + json[k].path + '" alt="">' +
                         '</a>';
 
 
