93065da78c24ab40752abe8096c656fbdcb6c65e
Paulo Bezerra <pauloh2012sul@gmail.com>
2021-01-10 22:10:21 -0300

    commit final

--- src/main/webapp/js/fotosList.js
+++ src/main/webapp/js/fotosList.js
@@ -52,6 +52,43 @@ function GetFotos(current) {
 
                     $("#messages").append(option_name);
 
+
+                    var modal = '<div id="modal' + json[k].id + '" class="modal fade bd-example-modal-lg show" tabindex="-1" role="dialog" aria-modal="true" style="padding-right: 17px;">' +
+                            '<div class="modal-dialog modal-lg" role="document">' +
+                            '<div class="modal-content">' +
+                            '<div class="modal-header">' +
+                            '<div class="row">' +
+                            '</div>' +
+                            '<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>' +
+                            '</div>' +
+                            '<div class="modal-body">' +
+                            '<div class="container-fluid">' +
+                            '<div class="row">' +
+                            '<div class="col-md-5 mb-2">' +
+                            '<img style="width: 100%;max-width: 200px;max-height: 200px;object-fit: cover;object-position: center;padding: 7px 0;border-radius: 50%;" src="https://www.cyberoficina.com.br' + json[k].path + '" alt="">' +
+                            //'<img style="width: 100%;max-width: 200px;max-height: 200px;object-fit: cover;object-position: center;padding: 7px 0;border-radius: 50%;" src="http://localhost:8080/Fotos_Festa' + json[k].path + '" alt="">'+
+                            '</div>' +
+                            '<div class="col-md-6 text-muted mt-2 mb-2">' +
+                            '<div class="row">' +
+                            '<h1 style=" font-size: 30px;  padding-bottom: 20px; color: #093970;">' + json[k].name + '</h1>' +
+                            '</div>' +
+                            '<div class="row">' +
+                            '<p>' + json[k].message + '</p>' +
+                            '</div>' +
+                            '</div>' +
+                            '</div>' +
+                            '</div>' +
+                            '</div>' +
+                            '<div class="modal-footer"></div>' +
+                            '</div>' +
+                            '</div>' +
+                            '</div>' +
+                            '</div>';
+
+                    $("#modalAppend").append(modal);
+
+
+
                     $('.toast').toast({delay: 20000});
                     $('.toast').toast('show');
 
